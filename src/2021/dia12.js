babe0b9023b3c4ac38a4d8cf2a8fcff8e1ad2c72
oddbytes <mack@oddbytes.net>
2021-12-12 19:43:37 +0100

    2021 dia 12

---
+++ src/2021/dia12.js
@@ -0,0 +1,10 @@
+function getMinJump(obstacles) {
+  //Devolver el menor numero que no es divisor de indices los obstaculos
+
+  // Version con arrays, menos eficiente porque calcula todos los saltos posibles para devolver finalmente el minimo
+  // return Math.min(...Array.from(Array(9).keys())
+  //   .map((x) => x + 1)
+  //   .filter((n) => !obstacles.some((o) => o % n == 0)));
+
+  for (let s = 2; s < 10; s++) if (!obstacles.some((o) => o % s == 0)) return s;
+}
